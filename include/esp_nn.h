131cf067906c6bed4793adbd21d1737c8b14d079
Vikram <vikram.dattu@espressif.com>
2022-05-30 13:51:52 +0530

    Implemented and used generic optimisations for non-esp32s3 chipsets
    Signed-off-by: Vikram <vikram.dattu@espressif.com>

--- include/esp_nn.h
+++ include/esp_nn.h
@@ -31,10 +31,12 @@ extern "C" {
 /* reference kernels included by default */
 #include "esp_nn_ansi_headers.h"
 
+#if defined(CONFIG_NN_OPTIMIZED)
 #if defined(ARCH_ESP32_S3)
 #include "esp_nn_esp32s3.h"
-#elif defined(ARCH_ESP32)
-#include "esp_nn_esp32.h"
+#else // for other platforms use generic optimisations
+#include "esp_nn_generic_opt.h"
+#endif // #if defined(ARCH_ESP32_S3)
 #else
 #include "esp_nn_ansi_c.h"
 #endif
